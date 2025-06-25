0e6547557796b72fcf4367aaa34c3d1fb6a0c2ed
babyboo99 <myduyentranthi2k4@gmail.com>
2025-06-25 17:58:55 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
-WEBHOOK_URL = "https://your-deployed-url.onrender.com"
+BOT_TOKEN = "8024370472:AAFBMFl0XbcNXrnDW6TqMZTBZ9Lj3tN7ofI"
+WEBHOOK_URL = "https://file-to-code.onrender.com"
