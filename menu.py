b0223efab944b848b6be1252b2f58c4b6f12c8c6
babyboo99 <myduyentranthi2k4@gmail.com>
2025-06-25 18:17:26 +0700

    Update menu.py

--- menu.py
+++ menu.py
@@ -4,5 +4,7 @@ def setup_persistent_menu(bot):
     bot.set_my_commands([
         BotCommand("pack", "Gửi nhiều media và lấy mã"),
         BotCommand("single", "Gửi tối đa 10 media và lấy mã"),
-    ], scope=BotCommandScopeDefault())
-    bot.set_chat_menu_button(menu_button=MenuButtonCommands())
+    ], scope=BotCommandScopeDefault()).
+async def setup_menu(bot):
+    await bot.set_my_commands([...])
+    await bot.set_chat_menu_button(MenuButtonCommands())
